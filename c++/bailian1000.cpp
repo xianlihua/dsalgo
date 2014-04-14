0007db5b7d5415b391937cf905e1a8f7f1ba44c7
xianlihua <xianlihua@gmail.com>
2014-04-14 22:30:56 +0800

    A+B problem

---
+++ c++/bailian1000.cpp
@@ -0,0 +1,21 @@
+/*
+描述
+Calculate a + b
+
+输入
+Two integer a,,b (0 ≤ a,b ≤ 10)
+输出
+Output a + b
+样例输入
+1 2
+样例输出
+3
+ */
+#include <iostream>
+using namespace std;
+int main () {
+    short a, b;
+    cin >> a >> b;
+    cout << (a + b);
+    return 0;
+}
