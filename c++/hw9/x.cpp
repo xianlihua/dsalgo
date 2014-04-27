22560336ea7b22e3bc2d97e884bbf3c5ce47bcb1
xianlihua <xianlihua@gmail.com>
2014-04-27 23:05:47 +0800

    思考题最后一题，不计分，供测试

--- c++/hw9/x.cpp
+++ c++/hw9/x.cpp
@@ -27,25 +27,25 @@ class CMyistream_iterator {
 };
 
 int main() {
-     int t;
-     cin >> t;
-     while (t--) {
-         CMyistream_iterator<int> inputInt(cin);
-         int n1,n2,n3;
-         n1 = * inputInt;
-         int tmp = * inputInt;
-         cout << tmp << endl;
-         inputInt ++; //
-         n2 = * inputInt;
-         inputInt ++; //
-         n3 = * inputInt;
-         cout << n1 << "," << n2<< "," << n3 << ",";
-         CMyistream_iterator<string> inputStr(cin);
-         string s1,s2;
-         s1 = * inputStr;
-         inputStr ++;
-         s2 = * inputStr;
-         cout << s1 << "," << s2 << endl;
+    int t;
+    cin >> t;
+    while (t--) {
+        CMyistream_iterator<int> inputInt(cin);
+        int n1,n2,n3;
+        n1 = * inputInt;
+        int tmp = * inputInt;
+        cout << tmp << endl;
+        inputInt ++; //
+        n2 = * inputInt;
+        inputInt ++; //
+        n3 = * inputInt;
+        cout << n1 << "," << n2<< "," << n3 << ",";
+        CMyistream_iterator<string> inputStr(cin);
+        string s1,s2;
+        s1 = * inputStr;
+        inputStr ++;
+        s2 = * inputStr;
+        cout << s1 << "," << s2 << endl;
     }
     return 0;
 }
